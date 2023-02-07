3f04d1cd03f1a0a5b3f1b423eca208d305b7dc1c
duart38 <duartsnel@icloud.com>
2023-02-07 15:55:51 +0100

    rename
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- bench/licenseCompute.ts
+++ bench/licenseCompute.ts
@@ -14,11 +14,10 @@
  *   limitations under the License.
  */
 
-// import { add } from "./main.ts";
 
-// Deno.bench(function addSmall() {
-//   add(1, 2);
-// });
+Deno.bench(function addSmall() {
+  add(1, 2);
+});
 
 // Deno.bench(function addBig() {
 //   add(2 ** 32, 2 ** 32);
