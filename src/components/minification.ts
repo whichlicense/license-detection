19a4a8be7bc3a0a71fd7cf07381a5464f91de1c3
duart38 <duartsnel@icloud.com>
2023-02-07 13:58:51 +0100

    add minification components
    Signed-off-by: duart38 <duartsnel@icloud.com>

---
+++ src/components/minification.ts
@@ -0,0 +1,23 @@
+/*
+ *   Copyright (c) 2023 Duart Snel
+
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+
+ *   http://www.apache.org/licenses/LICENSE-2.0
+
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+export function stripLicense(text: string): string {
+    return text.replaceAll(/( |\t|\n|\r|\n\r|\r\n)/g, "")
+}
+
+if(import.meta.main){
+    console.log(stripLicense(Deno.readTextFileSync(Deno.args[0])));
+}
