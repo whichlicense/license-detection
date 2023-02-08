deaa067341c989f9d8c52eb26a09bf16d290ae06
duart38 <duartsnel@icloud.com>
2023-02-08 12:47:24 +0100

    extracted license detection
    Signed-off-by: duart38 <duartsnel@icloud.com>

---
+++ src/components/detecting.ts
@@ -0,0 +1,55 @@
+/*
+ *   Copyright (c) 2023 Duart Snel
+
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+
+ *   http://www.apache.org/licenses/LICENSE-2.0
+
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+// TODO: make a manager that detects changes and imports them. Do this within a DAO class for better separation of concerns.
+import licenses from "../../licenses/ctph_hashes.json" assert { type: "json" };
+import { compareHashes, fuzzyHash } from "./hashing.ts";
+
+/**
+ * Attempts to detect the license of the incoming license text represented as a byte array.
+ * @param incomingLicense The license text, represented as a byte array
+ * @param confidenceThreshold The minimum confidence threshold for a match to be considered a match
+ * @returns an array of matches; empty array if no matches were found
+ */
+export function detectLicense(incomingLicense: Uint8Array, confidenceThreshold = 0.1){
+    /**
+    * Stores all the hash variations of the incoming license in a map, so we don't have to calculate them every time.
+    */
+    const incomingLicenseHashes = new Map<string, string>();
+    const matches: (ReturnType<typeof compareHashes> & {name: string})[] = [];
+
+    for(const entry in licenses){
+        const licenseName: keyof typeof licenses = entry as keyof typeof licenses;
+        const {blockSize, hash, fuzzyHashLength} = licenses[licenseName];
+    
+        // TODO: we can extract this to a global session-based cache? no need to calculate it multiple times in a single session
+        if(!incomingLicenseHashes.has(`${blockSize}-${fuzzyHashLength}`)){
+            incomingLicenseHashes.set(`${blockSize}-${fuzzyHashLength}`, fuzzyHash(incomingLicense, blockSize, fuzzyHashLength));
+        }
+    
+        const similarity = compareHashes(incomingLicenseHashes.get(`${blockSize}-${fuzzyHashLength}`)!, hash, fuzzyHashLength)
+        if(similarity.confidence > confidenceThreshold){
+          matches.push({
+            name: entry,
+            confidence: similarity.confidence,
+            commonBlocks: similarity.commonBlocks,
+            totalBlocks: similarity.totalBlocks
+          });
+        }
+    }
+
+    return matches;
+}
--- src/main.ts
+++ src/main.ts
@@ -14,46 +14,17 @@
  *   limitations under the License.
  */
 
-import { compareHashes, fuzzyHash } from "./components/hashing.ts";
 import { stripLicense } from "./components/minification.ts";
-
-import licenses from "../licenses/ctph_hashes.json" assert { type: "json" };
+import { detectLicense } from "./components/detecting.ts";
 
 
 // TODO: for testing purposes
 const licenseText = Deno.readTextFileSync('./TEST_LICENSE')
 const incomingLicense = new TextEncoder().encode(stripLicense(licenseText))
 
-/**
- * Stores all the hash variations of the incoming license in a map, so we don't have to calculate them every time.
- */
-const incomingLicenseHashes = new Map<string, string>();
-
-
-const matches: (ReturnType<typeof compareHashes> & {name: string})[] = [];
-
 const unclassified = JSON.parse(Deno.readTextFileSync('./licenses/unclassified.json'))
 
-
-
-for(const entry in licenses){
-    const licenseName: keyof typeof licenses = entry as keyof typeof licenses;
-    const {blockSize, hash, fuzzyHashLength} = licenses[licenseName];
-
-    if(!incomingLicenseHashes.has(`${blockSize}-${fuzzyHashLength}`)){
-        incomingLicenseHashes.set(`${blockSize}-${fuzzyHashLength}`, fuzzyHash(incomingLicense, blockSize, fuzzyHashLength));
-    }
-
-    const similarity = compareHashes(incomingLicenseHashes.get(`${blockSize}-${fuzzyHashLength}`)!, hash, fuzzyHashLength)
-    if(similarity.confidence > 0.1){
-      matches.push({
-        name: entry,
-        confidence: similarity.confidence,
-        commonBlocks: similarity.commonBlocks,
-        totalBlocks: similarity.totalBlocks
-      });
-    }
-}
+const matches = detectLicense(incomingLicense, 0.1)
 
 
 if(matches.length === 0){
