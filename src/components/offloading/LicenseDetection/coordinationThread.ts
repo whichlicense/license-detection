53cc8eb6ae673358722b10119e4370a0b7e9e328
duart38 <duartsnel@icloud.com>
2023-02-10 18:06:21 +0100

    add load system and simplified code
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- src/components/offloading/LicenseDetection/coordinationThread.ts
+++ src/components/offloading/LicenseDetection/coordinationThread.ts
@@ -22,24 +22,30 @@ import { TDetectionThreadReply,EDetectionThreadMessageType,ECoordinationThreadMe
 import LicenseStorage from "../../storage.ts";
 
 // TODO: how many workers does the user want to spawn? make this configurable. i believe we can use either env here or a config file.
-const detectionThreads = new Array<Worker>(4);
-
+const detectionThreads = new Array<Worker>(32);
 const DETECTIONS = new Map<string, {
     results: Array<TDetectionThreadReply['result']>
     progress: number
 }>();
 
+let LOAD_BUFFER: DataView | undefined;
+const detectionThreadRef = new URL("./detectionThread.ts", import.meta.url).href
 
 // spawn the threads
 for (let i = 0; i < detectionThreads.length; i++) {
-    detectionThreads[i] = new Worker(new URL("./detectionThread.ts", import.meta.url).href, { type: "module" });
+    detectionThreads[i] = new Worker(detectionThreadRef, { type: "module" });
+    
     detectionThreads[i].addEventListener("message", (e: MessageEvent<TDetectionThreadReply>) => {
         if(e.data.type === EDetectionThreadMessageType.RESULT){
             DETECTIONS.get(e.data.for)?.results.push(e.data.result);
             if(DETECTIONS.get(e.data.for)?.results.length === detectionThreads.length){
                 // all threads have sent the portion of their results. post the results to the main thread
+                 // TODO: results don't have an id, so we can't match them to the request.!!
+                 // TODO: extract into seperate function...
                 const REPLY: TCoordinationThreadMessage = {type: ECoordinationThreadMessageType.result, results: DETECTIONS.get(e.data.for)?.results.flatMap(x => x) ?? []};
                 postMessage(REPLY);
+                DETECTIONS.delete(e.data.for);
+                LOAD_BUFFER?.setUint32(0, DETECTIONS.size)
             }
         }
     });
@@ -54,8 +60,11 @@ const licenseDB = new LicenseStorage("./licenses/ctph_hashes.wlhdb");
 
 // TODO: pre-spawn the threads.. spawnup time can be slow.
 self.onmessage = (e: MessageEvent<TCoordinationThreadMessage>) => {
-
-    if(e.data.type === ECoordinationThreadMessageType.detect){
+    if(e.data.type === ECoordinationThreadMessageType.init){
+        const {loadBuffer} = e.data;
+        LOAD_BUFFER = new DataView(loadBuffer);
+        LOAD_BUFFER.setUint32(0, DETECTIONS.size)
+    }else if(e.data.type === ECoordinationThreadMessageType.detect){
         const {id, license} = e.data;
 
         // set up storage for request
@@ -65,28 +74,19 @@ self.onmessage = (e: MessageEvent<TCoordinationThreadMessage>) => {
                 return this.results.length / detectionThreads.length;
             }
         });
-
+        LOAD_BUFFER?.setUint32(0, DETECTIONS.size)
         const licenseCount = licenseDB.getEntryCount()
         const SHARED_LICENSE_BUFFER = new SharedArrayBuffer(e.data.license.byteLength);
         const RAW_LICENSE = new Uint8Array(SHARED_LICENSE_BUFFER);
         RAW_LICENSE.set(new Uint8Array(license));
-    
         // split licenseCount evenly between all worker threads. remainders will be put on the last worker thread
-        const licensesPerThread = Math.ceil(licenseCount / detectionThreads.length)
+        const licensesPerThread = Math.floor(licenseCount / detectionThreads.length)
         let currentThreadIndex = 0;
-        let threadLicenseIndex = 0; 
-    
-        // TODO: figure out if subsequent calls work.. pointer info might be lost here...
-    
         // would be nice if we can pre-group the licenses into chunks and send them to the threads.
         for(const licenseEntry of licenseDB.entriesBatched(licensesPerThread)){
-            threadLicenseIndex = (threadLicenseIndex + 1) % licensesPerThread;
             const MSG: TDetectionThreadMessage = {id, srcl: SHARED_LICENSE_BUFFER, db: licenseEntry.buffer}
             detectionThreads[currentThreadIndex]?.postMessage(MSG, [licenseEntry.buffer]);
-            if(threadLicenseIndex === 0){
-                // move to the next thread or wrap around to the first thread
-                currentThreadIndex = (currentThreadIndex + 1) % detectionThreads.length;
-            }
+            currentThreadIndex = (currentThreadIndex + 1) % detectionThreads.length;
         }
     }
 };
