4dd3608ca6dd66bbeebd16ea7b5ec102c6e6c76c
duart38 <duartsnel@icloud.com>
2023-02-10 13:56:16 +0100

    add method to detect license based on a raw db
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- src/components/detecting.ts
+++ src/components/detecting.ts
@@ -38,7 +38,8 @@ export function detectLicense(
   const matches: (ReturnType<typeof compareHashes> & { name: string })[] = [];
 
   for (const entry of licenseDB) {
-    const { blockSize, hash, hashLength, name } = LicenseStorage.parseEntry(entry);
+    // We know that this is only one entry, so we can safely destructure it here
+    const { blockSize, hash, hashLength, name } = LicenseStorage.parseEntry(entry).next().value!;
 
     // TODO: we can extract this to a global session-based cache? no need to calculate it multiple times in a single session
     if (!incomingLicenseHashes.has(`${blockSize}-${hashLength}`)) {
@@ -66,5 +67,51 @@ export function detectLicense(
   return matches;
 }
 
+/**
+ * Detects a license from a raw license database. This can be used to pass in partial databases (i.e., subsection of database).
+ * @param incomingLicense The license text, represented as a byte array
+ * @param confidenceThreshold The minimum confidence threshold for a match to be considered a match
+ * @returns an array of matches; empty array if no matches were found
+ */
+export function detectLicenseRawDB(
+  incomingLicense: TLicense,
+  rawLicenseDB: Uint8Array,
+  confidenceThreshold = 0.1,
+) {
+    // TODO: setting for early exit on high confidence match! (i.e., 100% should exit immediately)
+  /**
+   * Stores all the hash variations of the incoming license in a map, so we don't have to calculate them every time.
+   */
+  const incomingLicenseHashes = new Map<string, string>();
+  const matches: (ReturnType<typeof compareHashes> & { name: string })[] = [];
+
+  // We know that this is only one entry, so we can safely destructure it here
+  for(const { blockSize, hash, hashLength, name } of LicenseStorage.parseEntry(rawLicenseDB)){
+    // console.log(name, hash)
+    // TODO: we can extract this to a global session-based cache? no need to calculate it multiple times in a single session
+    if (!incomingLicenseHashes.has(`${blockSize}-${hashLength}`)) {
+      incomingLicenseHashes.set(
+        `${blockSize}-${hashLength}`,
+        fuzzyHash(incomingLicense, blockSize, hashLength),
+      );
+    }
+
+    const similarity = compareHashes(
+      incomingLicenseHashes.get(`${blockSize}-${hashLength}`)!,
+      hash,
+      hashLength,
+    );
+    if (similarity.confidence > confidenceThreshold) {
+      matches.push({
+        name,
+        confidence: similarity.confidence,
+        commonBlocks: similarity.commonBlocks,
+        totalBlocks: similarity.totalBlocks,
+      });
+    }
+  }
+
+  return matches;
+}
 // TODO: paralellize this? we can use deno sub-instances to do this. divide the licenseDB into chunks and then run each chunk in a separate instance.
 // we can also use threads instead of sub-instances.
