6e7f458b596c801eafe9f2111418a7d6d1b11198
duart38 <duartsnel@icloud.com>
2023-02-13 14:11:00 +0100

    re-export entire lib
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- src/main.ts
+++ src/main.ts
@@ -14,42 +14,33 @@
  *   limitations under the License.
  */
 
-import { stripLicense } from "./components/minification.ts";
-import { detectLicense } from "./components/detecting.ts";
+import LicenseStorage from "components/storage";
+export { LicenseStorage };
 
-// TODO: for testing purposes
-const licenseText = Deno.readTextFileSync("./TEST_LICENSE");
-const incomingLicense = new TextEncoder().encode(stripLicense(licenseText));
+export { DetectionScheduler } from "DetectionScheduler";
+export { detectLicense, detectLicenseRawDB } from "components/detecting";
+export { compareHashes, createHash, fuzzyHash } from "components/hashing";
+export { stripLicense } from "components/minification";
 
-const unclassified = JSON.parse(
-  Deno.readTextFileSync("./licenses/unclassified.json"),
-);
+export {
+  computeAllLicenseHashes,
+  computeLicenseHash,
+  DEFAULT_BLOCK_SIZE,
+  DEFAULT_FUZZY_HASH_LENGTH,
+  stripAndDumpLicense,
+} from "scripts/computeLicenses";
 
-const matches = detectLicense(incomingLicense, undefined, 0.1);
+export type {
+  ECoordinationThreadMessageType,
+  EDetectionThreadMessageType,
+  TCoordinationThreadMessage,
+  TDetectionResult,
+  TDetectionThreadMessage,
+} from "types/DetectionScheduler";
 
-if (matches.length === 0) {
-  console.log("No matches found");
-  // TODO: only store things like file path, not the whole license text. request the license text from the server when needed.
-  // TODO: maybe extract features from the license and store these to make lawyers happy
-  unclassified.push(licenseText);
-  console.log("Added to unclassified licenses\n\n", licenseText);
-} else {
-  // just printing out for testing
-  const bestMatch = matches.sort((a, b) => b.confidence - a.confidence);
-  console.log(`Found ${matches.length} matches.
-  The best match is ${bestMatch[0].name} with a confidence of ${
-    Math.min(100, bestMatch[0].confidence * 100)
-  }%
+export type { TLicense, TLicenseDBEntry } from "types/License";
 
-  All matches (ranked):
-  ${
-    matches
-      .map((match) =>
-        `${
-          Math.min(100, match.confidence * 100).toFixed(2)
-        }% - ${match.name} \t Common blocks: ${match.commonBlocks}/${match.totalBlocks}`
-      )
-      .join("\n")
-  }
-  `);
-}
+export type {
+  TLicenseComputeOptions,
+  TLicenseComputeSettingsOverride,
+} from "types/LicenseCompute";
