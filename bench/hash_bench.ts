24bfe99ff15371a56672622953558aa7bae3c5c9
duart38 <duartsnel@icloud.com>
2023-02-16 13:35:52 +0100

    add hash bench
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- bench/hash_bench.ts
+++ bench/hash_bench.ts
@@ -14,4 +14,90 @@
  *   limitations under the License.
  */
 
-// TODO: instead of the csv thingies, we can compare hashes just like we do in the csv thingy but with the accuracy in the title of the benchmark.
+import { compareHashes, fuzzyHash } from "components/hashing";
+import { stripLicense } from "components/minification";
+
+
+/**
+ * The maximum block size to test against. The program will test all block sizes from MIN_BLOCK_SIZE to this value.
+ */
+const MAX_BLOCK_SIZE = 25;
+const MIN_BLOCK_SIZE = 1;
+
+/**
+ * The maximum fuzzy hash length to test against. The program will test all fuzzy hash lengths from MIN_FUZZY_HASH_LENGTH to this value.
+ */
+const MAX_FUZZY_HASH_LENGTH = 25;
+const MIN_FUZZY_HASH_LENGTH = 1;
+
+/**
+ * Indicates if we should filter out results that are exited early (based on hash comparison threshold logic)
+ */
+const FILTER_OUT_DNF = true;
+
+const MIN_CONFIDENCE = 0.1;
+
+
+
+
+const ORIGINAL_FILE = stripLicense("@".repeat(8000));
+
+// Should generate a new string based on the original file with exactly half of the @ symbols replaced with # symbols.
+const halfOfFile = ORIGINAL_FILE.length / 2;
+function randomArrIndex<T>(arr: T[]): number {
+    return Math.floor(Math.random() * arr.length);
+}
+const CHAR_SPLIT = ORIGINAL_FILE.split("");
+for (let i = 0; i < halfOfFile; i++) {
+    const randomItem = randomArrIndex(CHAR_SPLIT);
+    if (CHAR_SPLIT[randomItem] === "@") {
+        CHAR_SPLIT[randomItem] = "#";
+    } else {
+        i--;
+    }
+}
+
+const HALF_MODIFIED_FILE = CHAR_SPLIT.join("");
+if (ORIGINAL_FILE.length !== HALF_MODIFIED_FILE.length) {
+    throw new Error(
+        "The files are not the same length!. the data will be... weird...",
+    );
+}
+
+
+
+
+for (let blockSize = MIN_BLOCK_SIZE; blockSize <= MAX_BLOCK_SIZE; blockSize++) {
+    for (
+        let fuzzyHashLength = MIN_FUZZY_HASH_LENGTH;
+        fuzzyHashLength <= MAX_FUZZY_HASH_LENGTH;
+        fuzzyHashLength++
+    ) {
+        const ORIGINAL_FILE_HASH = fuzzyHash(
+            new TextEncoder().encode(ORIGINAL_FILE),
+            blockSize,
+            fuzzyHashLength,
+        );
+        const CHANGED_FILE_HASH = fuzzyHash(
+            new TextEncoder().encode(HALF_MODIFIED_FILE),
+            blockSize,
+            fuzzyHashLength,
+        );
+
+        const comparingResults = compareHashes(
+            ORIGINAL_FILE_HASH,
+            CHANGED_FILE_HASH,
+        );
+        if (FILTER_OUT_DNF && comparingResults.commonBlocks === -1) continue;
+
+        Deno.bench(
+            `compareHashes [${blockSize}, ${fuzzyHashLength}] - Confidence: ${comparingResults.confidence}`,
+            {
+                group: "compareHashes",
+            },
+            () => {
+                compareHashes(ORIGINAL_FILE_HASH, CHANGED_FILE_HASH, MIN_CONFIDENCE);
+            },
+        );
+    }
+}
