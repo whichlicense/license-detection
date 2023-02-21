ad80f880901ad64b8db69ac61fcd185ceab5f71f
duart38 <duartsnel@icloud.com>
2023-02-21 14:45:48 +0100

    add test logic
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- tests/licenseCompute_test.ts
+++ tests/licenseCompute_test.ts
@@ -14,4 +14,36 @@
  *   limitations under the License.
  */
 
-// TODO: test that the computing of licenses works.
+import {
+assert,
+    assertEquals,
+  } from "https://deno.land/std@0.177.0/testing/asserts.ts";
+import { computeLicenseHash } from "scripts/computeLicenses";
+
+Deno.test("computeLicenseHash test", {}, async (t) => {
+    const EVEN_TEXT = "0000000000";
+    const EVEN_ENCODED = new TextEncoder().encode(EVEN_TEXT)
+
+    await t.step("blockSize 1 captures all", () => {
+        const res = computeLicenseHash(EVEN_ENCODED, 1, 10);
+        assertEquals(res.blockSize, 1);
+        assertEquals(res.fuzzyHashLength, 10);
+        assertEquals(res.hash.split(":").length, EVEN_TEXT.length);
+    });
+
+    await t.step("blockSize 2 captures in blocks of two", () => {
+        const res = computeLicenseHash(EVEN_ENCODED, 2, 10);
+        assertEquals(res.blockSize, 2);
+        assertEquals(res.fuzzyHashLength, 10);
+        assertEquals(res.hash.split(":").length, EVEN_TEXT.length / 2);
+        console.log(res.hash);
+    });
+
+    await t.step("low hash length clips", () => {
+        const res = computeLicenseHash(EVEN_ENCODED, 5, 1);
+        assertEquals(res.blockSize, 5);
+        assertEquals(res.fuzzyHashLength, 1);
+        // due to the radix we use, we can't be sure of the exact length. But it should be 1 or 2.
+        assert(res.hash.split(":").length <= 2);
+    });
+  });
