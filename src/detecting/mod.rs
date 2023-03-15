ced60772a89bba5c621de9266f98572dc8e68403
duart38 <duartsnel@icloud.com>
2023-03-15 15:08:58 +0100

    add diffing pipeline
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- src/detecting/mod.rs
+++ src/detecting/mod.rs
@@ -211,5 +211,53 @@ pub mod detecting {
                 }
             }
         }
+
+        pub mod diffing_pipeline {
+            use super::{ConditionalPipeline, RunnablePipeLineAction};
+            extern crate diff;
+
+            /// Diffing pipeline pipe which takes the contents
+            ///
+            /// If the regex matches, the given PipeLineAction is executed.
+            pub struct DiffingPipeLine {
+                /// The original license text as defined within our database
+                pub original_license: String,
+                /// The modified license text as fetched from the dependency
+                pub modified_license: String,
+                // The regex to be used in the matching of the diff between the original and modified license
+                pub regex: String,
+
+                /// The condition which determines if tis pipeline should be run or not
+                pub run_condition: super::PipelineTriggerInstruction,
+                /// The action to be executed if the regex matches
+                pub action: super::PipeLineAction,
+            }
+
+            impl super::RunnablePipeLine for DiffingPipeLine {
+                fn run(&self, confidence: u8) -> u8 {
+                    if self.run_condition.should_run(confidence) {
+                        let diff = diff::chars(&self.original_license, &self.modified_license);
+                        let mut diff_text = String::new();
+
+                        for line in diff {
+                            match line {
+                                diff::Result::Right(r) => {
+                                    diff_text.push(r);
+                                }
+                                _ => {}
+                            }
+                        }
+
+                        if regex::Regex::new(&self.regex).unwrap().is_match(&diff_text) {
+                            self.action.run(confidence)
+                        } else {
+                            confidence
+                        }
+                    } else {
+                        confidence
+                    }
+                }
+            }
+        }
     }
 }
