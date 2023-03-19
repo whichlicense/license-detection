bd232f65c9680e19d3c17b2f79d0d7876d08a960
duart38 <duartsnel@icloud.com>
2023-03-19 14:18:09 +0100

    moved detection related structs to its module
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- src/detecting/mod.rs
+++ src/detecting/mod.rs
@@ -16,10 +16,47 @@
 */
 
 pub mod fuzzy_implementation;
+pub mod gaoya_implementation;
+
 pub mod detecting {
+    use serde::{Serialize, Deserialize};
+
     #[derive(Debug)]
     pub struct LicenseMatch {
         pub name: String,
         pub confidence: u8,
+    }
+
+    #[derive(Serialize, Deserialize, Debug, Clone)]
+    pub struct ComputedLicense {
+        pub name: String,
+        pub hash: String,
+    }
+
+    #[derive(Serialize, Deserialize, Debug, Clone)]
+    pub struct ComputedLicenseList {
+        pub licenses: Vec<ComputedLicense>,
+    }
+    pub trait LicenseListActions {
+        /// Converts the plain text into a representation that can be used to find a license
+        /// then runs the match_by_hash function on that representation.
+        fn match_by_plain_text(&self, plain_text: String) -> Vec<LicenseMatch>;
+
+        /// Attempts to find one or more matching licenses by hash.
+        fn match_by_hash(&self, hash: String) -> Vec<LicenseMatch>;
+
+        /// Saves the computed license list to a file.
+        fn save_to_file(&self, file_path: String);
+
+        /// Loads a computed license list from a file and stores it in the hosting struct.
+        fn load_from_file(&mut self, file_path: String);
+
+        /// Adds a license that has yet to be computed to the list.
+        /// 
+        /// This license must be in plain text format.
+        fn add_plain(&mut self, license_name: String, license_text: String);
+
+        /// Removes a license from the list.
+        fn remove(&mut self, license_name: String);
     }
 }
--- src/license_tools/mod.rs
+++ src/license_tools/mod.rs
@@ -18,43 +18,6 @@
 pub mod license_tools {
     use std::{fs::{self, File}, io::Read};
     use regex::Regex;
-    use serde::{Deserialize, Serialize};
-
-    use crate::LicenseMatch;
-
-
-    #[derive(Serialize, Deserialize, Debug, Clone)]
-    pub struct ComputedLicense {
-        pub name: String,
-        pub hash: String,
-    }
-
-    #[derive(Serialize, Deserialize, Debug, Clone)]
-    pub struct ComputedLicenseList {
-        pub licenses: Vec<ComputedLicense>,
-    }
-    pub trait LicenseListActions {
-        /// Converts the plain text into a representation that can be used to find a license
-        /// then runs the match_by_hash function on that representation.
-        fn match_by_plain_text(&self, plain_text: String) -> Vec<LicenseMatch>;
-
-        /// Attempts to find one or more matching licenses by hash.
-        fn match_by_hash(&self, hash: String) -> Vec<LicenseMatch>;
-
-        /// Saves the computed license list to a file.
-        fn save_to_file(&self, file_path: String);
-
-        /// Loads a computed license list from a file and stores it in the hosting struct.
-        fn load_from_file(&mut self, file_path: String);
-
-        /// Adds a license that has yet to be computed to the list.
-        /// 
-        /// This license must be in plain text format.
-        fn add_plain(&mut self, license_name: String, license_text: String);
-
-        /// Removes a license from the list.
-        fn remove(&mut self, license_name: String);
-    }
 
     pub fn strip_spdx_heading(l: &str) -> String {
         // TODO: return a vector with the groups in one slot and the replaced string in the other
